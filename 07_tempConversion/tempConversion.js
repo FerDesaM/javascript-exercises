122680df52530a63be219809d7ffeb93063d79d3
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-08 17:21:48 -0500

    First test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(grade) {
+  let celsius=(grade-32)*5/9;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(grade) { 
+  let fahrenheit;
+
 };
 
 // Do not edit below this line
