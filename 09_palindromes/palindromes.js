86be2d5d847d082e16c1fb6f7e8cd76a46b49695
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-16 11:52:52 -0500

    All test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const panlindrome=word.replace(/[^a-zA-Z1-9]/g,'').toLowerCase();
+    for(let i=word.length-1;i>0;i--){
+        if(panlindrome[panlindrome.length-1-i]!==panlindrome[i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
