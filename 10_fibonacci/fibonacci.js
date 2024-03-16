d8ed423a915bcfbb61d5c1fe1c5fa9adb77f5502
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-16 12:21:39 -0500

    All test passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let getPos=parseInt(position);
+    if(getPos<0)
+        return "OOPS";
+    if(getPos===0)
+        return 0;
+    if(getPos===1)
+        return 1;
+    else
+        return fibonacci(getPos-1)+fibonacci(getPos-2)
 };
 
 // Do not edit below this line
