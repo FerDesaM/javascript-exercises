0fb50094e256c863c508cc3f8809223b5190a0c4
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-16 11:29:45 -0500

    All test passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(first ,second) {
+	return first+second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first,second) {
+	return first-second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,currentItem)=>total+currentItem,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,currentItem)=>total*currentItem,1)
 };
 
-const power = function() {
-	
+const power = function(first,second) {
+	return first**second;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number===0)
+    return 1;
+  else
+	return number*(factorial(number-1))
 };
 
 // Do not edit below this line
