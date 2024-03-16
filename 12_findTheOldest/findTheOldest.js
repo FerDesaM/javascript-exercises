c644cfe2f5d1420e29b8e4a34f33377f7bcf1c2a
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-16 12:49:29 -0500

    all test passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(Person) {
+    return Person.reduce((oldest,younger)=>{
+    const Oldest=AgeCurrent(oldest.yearOfBirth,oldest.yearOfDeath);
+    const Actual=AgeCurrent(younger.yearOfBirth,younger.yearOfDeath);
+        return Oldest<Actual?younger:oldest;
+}); 
 };
+
+const AgeCurrent=function(birth,death){
+    if(!death){
+        death=new Date().getFullYear();
+    }
+    return death-birth;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
